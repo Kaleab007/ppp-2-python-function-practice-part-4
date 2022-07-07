893b37fbe4b0b81004db092ac3bfcfcade10e6cd
Kaleab007 <abatenehkaleab@gmail.com>
2022-07-07 00:00:10 -0700

    num

---
+++ main.py/main.py
@@ -0,0 +1,46 @@
+from calendar import c
+from re import A
+
+
+#max_num ()
+
+
+#def max_num():
+    return max ([a,b,c])
+
+    print(max_num(1,2,3))
+juuj
+u
+print(max_num(1,2,3)) 
+print(max_num(100,50,1))
+print(max_num(15,30,2))
+
+def multi_list(1st):
+
+    if len(1st) == 0
+    return 0
+    prod = 1st[0]
+
+for i in "1st[1:]:":
+    prod = prod *i
+    "return":prod
+
+    print (multi_list([1,2,3]))
+    print (multi_list([]))  
+    print (multi_list([15]))
+
+def rev_string(my_str):
+    return my_str[::-1]
+
+    print (rev_string("")) 
+    print (rev_string("apple"))
+    print(rev_string("mt string"))
+
+    def num_within(x,a,b):
+        return x in range(a,b+1)
+
+        print(num_within(3,2,4))
+        print(num_within(3,1,3))
+        print(numb_within(10,2,5))
+        
+
