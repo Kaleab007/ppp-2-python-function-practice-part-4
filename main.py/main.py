5d9b15b9ea57366dcd0030e394ddc54a85c82e4c
Kaleab007 <abatenehkaleab@gmail.com>
2022-07-07 00:24:49 -0700

    row

--- main.py/main.py
+++ main.py/main.py
@@ -5,42 +5,60 @@ from re import A
 #max_num ()
 
 
-#def max_num():
-    return max ([a,b,c])
+def max_num():
+    return max([a, b, c])
 
-    print(max_num(1,2,3))
-juuj
-u
-print(max_num(1,2,3)) 
-print(max_num(100,50,1))
-print(max_num(15,30,2))
 
-def multi_list(1st):
+print(max_num(1, 2, 3))
+print(max_num(100, 50, 1))
+print(max_num(15, 30, 2))
 
-    if len(1st) == 0
-    return 0
-    prod = 1st[0]
 
-for i in "1st[1:]:":
-    prod = prod *i
-    "return":prod
+def mult_list(lst):
 
-    print (multi_list([1,2,3]))
-    print (multi_list([]))  
-    print (multi_list([15]))
+    if len(lst) == 0:
+        return 0
+
+    prod = lst[0]
+
+    if len(lst) > 1:
+        for i in lst[1:]:
+            prod = prod * i
+    return prod
+
+
+print(mult_list([1, 2, 3]))
+print(mult_list([]))
+print(mult_list([15]))
 
 def rev_string(my_str):
     return my_str[::-1]
 
-    print (rev_string("")) 
-    print (rev_string("apple"))
-    print(rev_string("mt string"))
+print(rev_string(""))
+print(rev_string("apple"))
+print(rev_string("mt string"))
 
-    def num_within(x,a,b):
-        return x in range(a,b+1)
+def num_within(x, a, b):
+    return x in range(a,b+1)
 
-        print(num_within(3,2,4))
-        print(num_within(3,1,3))
-        print(numb_within(10,2,5))
-        
+print(num_within(3, 2, 4))
+print(num_within(3, 1, 3))
+print(num_within(10, 2, 5))
+
+
+triangle = [[1], [1, 1]]
+
+
+def pascal(n):
+
+    if n < 1:
+
+        print("invalid number of rows")
+    elif n == 1:
+        print(triangle[0])
+    else:
+        row_number=2
+        while len(triangle) < n:
+            row = []
+            row_prev = triangle[row_number - 1 ]
 
